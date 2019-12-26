d279430f6af4a4d106a6cefe6b951ee699b9e7bc
get-a-clue <trafficreg@gmail.com>
2019-12-26 21:43:27 +0200

    Initial commit

---
+++ hello.py
@@ -0,0 +1,28 @@
+import time
+import msvcrt as m
+import sys
+from instahelpers import *
+
+instagram_login = ""
+instagram_password = ""
+
+filename = "likebot_" + instagram_login + ".log"
+logging.basicConfig(filename=filename, format = '%(levelname)-8s [%(asctime)s]  %(message)s', level = logging.INFO)
+
+while True:
+   logging.info("Application started")
+   idriver = InstaDriver(True, instagram_login, instagram_password)
+   idriver.go_url("https://instagram.com")
+
+   driver = idriver.driver
+   assert "Instagram" in driver.title
+   idriver.suppress_notifications()
+
+   if idriver.check_already_logged_user() is False:
+       if idriver.do_login() is False:
+          sys.exit(1)
+       idriver.suppress_notifications()
+
+   idriver.do_like_newsfeed(50)
+   driver.quit()
+   time.sleep(3600)
