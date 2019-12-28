3ea1f55ac5aecf55f19f40f1451de9b0830a124c
get-a-clue <trafficreg@gmail.com>
2019-12-28 10:43:46 +0200

    minor tweaks

--- hello.py
+++ hello.py
@@ -1,5 +1,3 @@
-import time
-import msvcrt as m
 import sys
 from instahelpers import *
 
@@ -9,20 +7,18 @@ instagram_password = ""
 filename = "likebot_" + instagram_login + ".log"
 logging.basicConfig(filename=filename, format = '%(levelname)-8s [%(asctime)s]  %(message)s', level = logging.INFO)
 
-while True:
-   logging.info("Application started")
-   idriver = InstaDriver(True, instagram_login, instagram_password)
-   idriver.go_url("https://instagram.com")
+logging.info("Application started")
+idriver = InstaDriver(True, instagram_login, instagram_password)
+idriver.go_url("https://instagram.com")
 
-   driver = idriver.driver
-   assert "Instagram" in driver.title
-   idriver.suppress_notifications()
+driver = idriver.driver
+assert "Instagram" in driver.title
+idriver.suppress_notifications()
 
-   if idriver.check_already_logged_user() is False:
-       if idriver.do_login() is False:
-          sys.exit(1)
-       idriver.suppress_notifications()
+if idriver.check_already_logged_user() is False:
+    if idriver.do_login() is False:
+       sys.exit(1)
+    idriver.suppress_notifications()
 
-   idriver.do_like_newsfeed(50)
-   driver.quit()
-   time.sleep(3600)
+idriver.do_like_newsfeed(50)
+driver.quit()
